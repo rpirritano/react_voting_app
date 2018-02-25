cd3b94129870aade5d14b24ae4c019807d48f1ce
Robert Pirritano <rpirritano@gmail.com>
2018-02-25 10:49:55 -0800

    used property initializers

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,15 @@
 /* eslint-disable no-param-reassign, operator-assignment */
 
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: [],
-    };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
-  }
+  state = {
+    products: [],
+  };
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  // Inside `ProductList`
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
@@ -57,15 +50,9 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
 
   render() {
     return (
